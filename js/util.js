7b2e78c69ca34dfe125899b18c6bd7f0c5759521
Aleksandr <markovkin88@gmail.com>
2019-10-10 22:54:39 +0300

    Добавляет модуль чтения и загрузки данных на сервер.

--- js/util.js
+++ js/util.js
@@ -1,11 +1,8 @@
 'use strict';
 
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
-
   window.util = {
-    ESC_KEYCODE: ESC_KEYCODE,
-    ENTER_KEYCODE: ENTER_KEYCODE,
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
   };
 })();
